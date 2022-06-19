9386be4361500717bc9c4cf1cf5faf1f71708ee4
Madhusudan0906 <prajapatmadhusudan06@gmail.com>
2022-06-19 14:39:52 +0530

    added menu button

---
+++ menu.js
@@ -0,0 +1,21 @@
+window.addEventListener('load',function(){
+    document.querySelector('#menuDiv').style.display='none';
+});
+document.querySelector('#menu').addEventListener('click',function(){
+    document.querySelector('#menuDiv').style.display='block';
+    document.querySelector('#menuDiv').style.backgroundColor='#2a5e88';
+    document.querySelector('#tp').style.display='none';
+    
+});
+document.querySelector('#startClose').addEventListener('click',function(){
+    document.querySelector('#menuDiv').style.display='none';
+    document.querySelector('#tp').style.display='block';
+    document.querySelector('body').style.backgroundColor='#f5f5f5';
+    window.location.reload();
+});
+document.querySelector('#endClose').addEventListener('click',function(){
+    document.querySelector('#menuDiv').style.display='none';
+    document.querySelector('#tp').style.display='block';
+    document.querySelector('body').style.backgroundColor='#f5f5f5';
+    window.location.reload();
+});
